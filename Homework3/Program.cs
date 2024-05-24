7beae181b132191588a06b069fa9b321d961a2a4
maksb <maksb@MAKSPC>
2024-05-24 23:07:34 +0300

    v5

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -89,6 +89,87 @@ void Sc2()
 
 void Sc3()
 {
+    void errormessage(string text)
+    {
+        Console.Clear();
+        Console.WriteLine(text);
+        Sc3();
+    }
+
+    try
+    {
+        Console.WriteLine("Enter two numbers");
+
+        Console.Write("> ");
+        string inp1 = Console.ReadLine();
+
+        Console.Write("> ");
+        string inp2 = Console.ReadLine();
+
+        int first = Convert.ToInt32(inp1);
+        int second = Convert.ToInt32(inp2);
+
+        Console.WriteLine("Enter arithmetic operation");
+
+        int Plus(int a,int b)
+        {
+            return a + b;
+        }
+        int Minus(int a, int b)
+        {
+            return b - a;
+        }
+        float Divide(float a, float b)
+        {
+            return b / a;
+        }
+        float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+
+        string[] Operations = { "+", "-", "*", "/" };
+        foreach (string item in Operations)
+        {
+            Console.WriteLine("|{0,3}  |", item);
+        }
+        Console.Write("> ");
+        string op = Console.ReadLine();
+
+      
+
+        if (Operations.Contains(op))
+        {
+            if (op == "+")
+            {
+                Console.WriteLine(Plus(first,second));
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(Minus(first, second));
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(Multiply(first, second));
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(Divide(first, second));
+            }
+            
+
+        }
+        else
+        {
+            errormessage("Error Invalid operator");
+        }
+    }
+    catch (Exception)
+    {
+        errormessage("Error");
+    }
+   
+
 
 }
 
